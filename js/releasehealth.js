15f327f63e55972464eeac342a6d0dc4ef5b7b60
F3real <stefan92ff@yandex.com>
2016-05-13 00:07:20 +0200

    Show unmigrated builders

--- js/releasehealth.js
+++ js/releasehealth.js
@@ -3,12 +3,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-var BIG_SCREEN = "bigscreen";
-var SMALL_SCREEN = "smallscreen";
-
 var versions;
-
+var repo_name;
 
 $(document).ready(function () {
   $.getJSON('js/bzconfig.json', function(data) {
@@ -18,26 +14,16 @@ $(document).ready(function () {
 
 function main(bzconfig) {
   versions = bzconfig.versions;
-  var display = getDisplay();
 
   displayTitle();
 
-  if (display !== BIG_SCREEN) {
-    displayForkOnGitHub();
-  }
+  repo_name = getRepoName();
 
+  //TODO figure a way to generate and load graph instead of loading it from file
+  $.getJSON('js/graph.json', display_json_results).error(function(){
+     $("#content").append("<div> <p>Error: Failed to fetch JSON data.<p></div>");
+  });
 
-
-  // to do
-  //window.setInterval(getBugCounts, bzconfig.refreshMinutes * 60 * 1000, version);
-}
-
-function getDisplay() {
-  var display = $.url().param('display');
-  if (display && (display === BIG_SCREEN)) {
-    return BIG_SCREEN;
-  }
-  return SMALL_SCREEN;
 }
 
 function displayTitle() {
@@ -48,8 +34,63 @@ function displayTitle() {
   $("#header-bg").attr("class", "header-bg header-bg-" + channel);
 }
 
+function getRepoName() {
+  var repo_name = $.url().param('repo_name');
+  if (repo_name){
+    return repo_name;
+  }
+  return "mozilla-central";
+}
 
 
-function displayForkOnGitHub(){
-  $("#body").append("<span id=\"forkongithub\"><a href=\"https://github.com/mozilla/ReleaseHealth\">Fork me on GitHub</a></span>");
+function display_json_results(graph){
+  var list_sufix = "lst";
+  $("#content").prepend("<h1>"+repo_name+"</h1>");
+  for(var key in graph){
+    $("#platforms").append("<div id="+key+"><section><h3>"+key+"</h3><ul id="+key+list_sufix+"></ul></section></div>");
+
+    for(platform in graph[key]){
+
+      if(repo_name_count(key, platform, repo_name)=== 0){
+        $("#"+key+list_sufix).append("<p>"+platform.strike()+"</p>");
+      }
+      else{
+        $("#"+key+list_sufix).append("<p class="+key+">"+platform+"</p>");
+      }
+    }
+  }
+
+  $("#platforms").on('click', 'p', function(){
+
+    var platform =$(this).text();
+    var key = $(this).attr('class');
+
+    $("#builders").html("");
+    if( key == null){
+      $("#builders").append("<h3> No unimgrated builders corresponding to "+repo_name+" repo name</h3>");
+      console.log("add")
+    }
+    else {
+      for(builder in graph[key][platform]){
+        if(builder.indexOf(name) !== -1){
+          $("#builders").append("<p>"+builder+"</p>");
+        }
+      }
+    }
+
+
+
+  });
+
+  //helper funcion to show wich platforms have no builders responding to search
+  function repo_name_count(key, platform, name){
+    var count = 0;
+    for(builder in graph[key][platform]){
+      if(builder.indexOf(name) !== -1){
+        count +=1;
+      }
+    }
+    return count;
+  }
+
 }
